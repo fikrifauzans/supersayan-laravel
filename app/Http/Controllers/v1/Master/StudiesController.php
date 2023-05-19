18abed410a434f5b22abf801340367f42682f72a
Fikri Fauzan <fikrifauzans.goku@gmail.com>
2023-05-19 13:04:57 +0700

    push

--- app/Http/Controllers/v1/Master/StudiesController.php
+++ app/Http/Controllers/v1/Master/StudiesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\V1\Master;
+namespace App\Http\Controllers\v1\Master;
             
 use App\Http\Controllers\V1\Auth\Controller;
 use App\Repositories\V1\Eloquent\Master\StudiesRepository;
