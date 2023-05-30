966473decd9dea9f58e6e6896779122362dfc025
Fikri Fauzan <fikrifauzans.goku@gmail.com>
2023-05-30 14:55:55 +0700

    fixing

--- interface/src/router/generator/generator.js
+++ interface/src/router/generator/generator.js
@@ -1,36 +1,7 @@
 
 export default [
 
-    
-           
-   { name: 'schools', path: 'schools', component: () => import('pages/master/schools/index.vue') },
-   { name: 'add-schools', path: 'schools/form', component: () => import('pages/master/schools/form.vue') },
-   { name: 'edit-schools', path: 'schools/form/:id', component: () => import('pages/master/schools/form.vue') },
-   { name: 'view-schools', path: 'schools/:id', component: () => import('pages/master/schools/detail.vue') },
-
-           
-   { name: 'studies', path: 'studies', component: () => import('pages/master/studies/index.vue') },
-   { name: 'add-studies', path: 'studies/form', component: () => import('pages/master/studies/form.vue') },
-   { name: 'edit-studies', path: 'studies/form/:id', component: () => import('pages/master/studies/form.vue') },
-   { name: 'view-studies', path: 'studies/:id', component: () => import('pages/master/studies/detail.vue') },
-
-           
-   { name: 'classes', path: 'classes', component: () => import('pages/master/classes/index.vue') },
-   { name: 'add-classes', path: 'classes/form', component: () => import('pages/master/classes/form.vue') },
-   { name: 'edit-classes', path: 'classes/form/:id', component: () => import('pages/master/classes/form.vue') },
-   { name: 'view-classes', path: 'classes/:id', component: () => import('pages/master/classes/detail.vue') },
-
-           
-   { name: 'lesson-timetable', path: 'lesson-timetable', component: () => import('pages/master/lesson-timetable/index.vue') },
-   { name: 'add-lesson-timetable', path: 'lesson-timetable/form', component: () => import('pages/master/lesson-timetable/form.vue') },
-   { name: 'edit-lesson-timetable', path: 'lesson-timetable/form/:id', component: () => import('pages/master/lesson-timetable/form.vue') },
-   { name: 'view-lesson-timetable', path: 'lesson-timetable/:id', component: () => import('pages/master/lesson-timetable/detail.vue') },
-
-           
-   { name: 'presences', path: 'presences', component: () => import('pages/master/presences/index.vue') },
-   { name: 'add-presences', path: 'presences/form', component: () => import('pages/master/presences/form.vue') },
-   { name: 'edit-presences', path: 'presences/form/:id', component: () => import('pages/master/presences/form.vue') },
-   { name: 'view-presences', path: 'presences/:id', component: () => import('pages/master/presences/detail.vue') },
+   
 
            
    { name: 'contents', path: 'contents', component: () => import('pages/master/contents/index.vue') },
