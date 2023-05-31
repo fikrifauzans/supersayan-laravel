6ee3805735b63bc6dcd978b7fe85ad8f315a697d
Fikri Fauzan <fikrifauzans.goku@gmail.com>
2023-06-01 00:44:52 +0700

    push

--- interface/src/Services/System/index.js
+++ interface/src/Services/System/index.js
@@ -3,15 +3,15 @@ class System {
   // API CONFIG
   apiRoot() {
     // return process.env.API_BASE_URL ?? 'https://api.beytcoffeetigabelas.com'
-    if (window.location.origin == 'http://localhost:8080') {
-      return 'http://localhost:8090/'
-      // return 'https://api.beytcoffeetigabelas.com/'
-      // return 'https://api.ums.dutabaitullah.com/'
-    }
-    // else return 'api.ums.dutabaitullah.com/'
-    else return 'https://ec-api.beytcoffeetigabelas.com/'
+    // if (window.location.origin == 'http://localhost:8080') {
+    //   return 'http://localhost:8090/'
+    //   // return 'https://api.beytcoffeetigabelas.com/'
+    //   // return 'https://api.ums.dutabaitullah.com/'
+    // }
+    // // else return 'api.ums.dutabaitullah.com/'
+    // else return 'https://ec-api.beytcoffeetigabelas.com/'
     // else return 'http://localhost:8090/'
-    // return 'https://ec-api.beytcoffeetigabelas.com/'; 
+    return 'https://dev.maghfirah.com/'; 
   }
   apiTimeout() { return 10000 }
 
